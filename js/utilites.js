8ce4a18919b9a11d1a474fa7d694b8a73311ee93
KaziAbdurRahim <kaziabdurrahim946@gmail.com>
2024-09-23 03:00:23 +0600

    added the blog to home page and the utilities functions

--- js/utilites.js
+++ js/utilites.js
@@ -1,1 +1,22 @@
 // common functions.
+
+// input value
+
+function getInputFieldValueByID (Id){
+    const inputValue=document.getElementById(Id).value;
+    const inputNumber= parseFloat(inputValue);
+    return inputNumber;
+}
+
+// innerText value
+function getInnerTextValueByID(Id){
+    const innerTextValue=document.getElementById(Id).innerText;
+    const innerTextNumber= parseFloat(innerTextValue);
+    return innerTextNumber;
+
+}
+
+function addMoneyToIInnerText(Addmoney,Id){
+    document.getElementById(Id).innerText = Addmoney;
+    
+}
